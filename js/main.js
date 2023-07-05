4adab0283d1c99a2d734bbbedcb50005d11959d6
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-05 15:12:16 +0200

    file creation + form html

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Usiamo un input in html per inserire la mail da cercare.
+*/ 
+
+// creo una costante dell'email dell'user
+const userEmail = document.getElementById("usere-email");
+// creo un array con tutte le mail conosciute 
+const allEmail = ["gio@g.com", "vane@g.com", "marco@g.com", "luigi@g.com"];
+// un ciclo for per confrontora le mail conosciute con quella inserita
+for (i = 0; i < allEmail; i++) {
+    console.log()
+}
