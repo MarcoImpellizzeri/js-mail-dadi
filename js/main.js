87a3f9aa067d3c03be487ec4cd62381273dd825b
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-05 16:41:38 +0200

    exercise mail done

--- js/main.js
+++ js/main.js
@@ -4,17 +4,39 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 Usiamo un input in html per inserire la mail da cercare.
-*/ 
+*/
 
 // creo una costante dell'email dell'user
 const userEmailInputElement = document.querySelector("#usere-email");
-// creo una costante per il button 
-const buttonVerify = document.querySelector("#btn-verify")
+
+// creo le costanti per i button 
+const buttonVerify = document.querySelector("#btn-verify");
+const buttonReload = document.querySelector("#btn-reload");
+
+// creo un array per le mie varie email
+const allEmail = ["vane@g.com", "luigi@g.com", "piero@g.com", "marco@g.com"];
 
 // con ".addEventListener" faccio si che quando premo il mio butto "buttonVerify"
 // agisce facendo un print in console della mail che inserisco
 buttonVerify.addEventListener("click", function () {
-    let userEmail = userEmailInputElement.value
+    let userEmail = userEmailInputElement.value;
+    const mailVerification = userEmail;
+    
+    // creo un ciclo for per verificare se la mia mail si trova all'interno
+    // del mi array, quindi controllo che sia valida
+    for (let i = 0; i < allEmail.length; i++) {
+        const currentEmail = allEmail[i];
+        const titleDenger = document.getElementById("validation-answer");
 
-    console.log(userEmail)
+        if (currentEmail === mailVerification) {
+            document.getElementById("validation-answer").innerHTML = `La tua e-mail è valida`;
+        } else {
+            document.getElementById("validation-answer").innerHTML = `La tua e-mail non è valida`;
+            titleDenger.classList.add("text-danger");
+        }
+    }
+})
+
+buttonReload.addEventListener("click", function(){
+    location.reload();
 })
